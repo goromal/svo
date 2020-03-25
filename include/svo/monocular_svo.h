6e2c71cab2239e36a2761167f56479a87ea51c4e
Andrew Torgesen <andrew.torgesen@gmail.com>
2020-03-24 21:46:36 -0400

    add ostream operator overload for MonocularSVO class

--- include/svo/monocular_svo.h
+++ include/svo/monocular_svo.h
@@ -18,11 +18,21 @@ public:
     bool initialize(const std::string &cam_model, const std::vector<double> &cam_params);
     void update(const cv::Mat &img, const double &t);
     void getPose(Eigen::Vector3d &trans, Eigen::Matrix3d &Rot);
+    friend ostream& operator <<(ostream& os, const MonocularSVO& msvo);
     ~MonocularSVO();
 private:
     bool initialized_;
     FrameHandlerMono* vo_;
     vk::AbstractCamera* cam_;
 };
+
+std::ostream& operator<< (std::ostream& os, const MonocularSVO& msvo)
+{
+    if (msvo.initialized_)
+        os << "Initialized ";
+    else
+        os << "Uninitialized ";
+    os << "monocular SVO solver with image (w=" << msvo.cam_->width() << ", h=" << msvo.cam_->height() << ")";
+}
 
 } // end namespace svo
